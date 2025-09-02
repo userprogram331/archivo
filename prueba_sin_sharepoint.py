30ae5a42de0109ee477e4c8f6ac405cf40526038
userprogram331 <userprogram33@tutamail.com>
2025-09-02 17:39:30 -0600

    Update prueba_sin_sharepoint.py

--- prueba_sin_sharepoint.py
+++ prueba_sin_sharepoint.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import json
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from datetime import datetime
 import ssl
